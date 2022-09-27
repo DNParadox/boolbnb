2873a841962697853ffbdbb45ba67995391b2992
LorenzoVeropalumbo <lorenzo-veropalumbo@outlook.it>
2022-09-27 13:16:15 +0200

    fix

--- database/migrations/2022_09_26_171845_create_sponsorships_table.php
+++ database/migrations/2022_09_26_171845_create_sponsorships_table.php
@@ -17,7 +17,7 @@ class CreateSponsorshipsTable extends Migration
             $table->id();
             $table->char('name', 100);
             $table->smallInteger('hour');
-            $table->
+            // $table->
             $table->timestamps();
         });
     }
