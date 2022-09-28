9a82dc5f9dfc9be5eb2dc7cb669414c016984794
AngeloDalicandro <angelo.dalicandro1@gmail.com>
2022-09-28 10:03:29 +0200

    homepage and pagenotfound added to vuerouter

--- resources/js/router.js
+++ resources/js/router.js
@@ -3,8 +3,24 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter);
 
+
+import HomePage from './pages/HomePage.vue';
+import NotFound from './pages/NotFound.vue';
+
 const router=new VueRouter({
-    mode:'history'
+    mode:'history',
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomePage
+        },
+        {
+            path: '/*',
+            name: 'not-found',
+            component: NotFound
+        }
+    ]
 });
 
 export default router;
