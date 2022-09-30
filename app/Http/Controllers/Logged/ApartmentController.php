3416de9a59e8dffe6f885da5da45cd9d2c47bb5f
LorenzoVeropalumbo <lorenzo-veropalumbo@outlook.it>
2022-09-30 11:42:33 +0200

    2 separete validation for create and edit

--- app/Http/Controllers/Logged/ApartmentController.php
+++ app/Http/Controllers/Logged/ApartmentController.php
@@ -82,7 +82,7 @@ class ApartmentController extends Controller
      */
     public function store(Request $request)
     {
-        $request->validate($this->getValidationRules());
+        $request->validate($this->getValidationRulesCreate());
         $form_data = $request->all();
         $user = Auth::user();
         if(isset($form_data['photo'])) {
@@ -165,7 +165,7 @@ class ApartmentController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $request->validate($this->getValidationRules()); 
+        $request->validate($this->getValidationRulesEdit()); 
         $form_data = $request->all();
         $old_apartment = Apartment::findOrFail($id);
         
@@ -212,7 +212,7 @@ class ApartmentController extends Controller
     }
 
 
-    protected function getValidationRules() {
+    protected function getValidationRulesCreate() {
         // Creo le Validazioni per i campi nel form
         return [
             'title' => 'required|min:5|max:100',
@@ -221,7 +221,23 @@ class ApartmentController extends Controller
             'bathroom' => 'required|min:1|max:999|numeric',
             'address' => 'required|min:1|max:60000',
             'price' => 'min:0|max:9999999|numeric|nullable',
-            'photo' => 'required|image|max: 1024|nullable',
+            'photo' => 'required|image|max: 1024',
+            'description' => 'min:1|max:60000|nullable',
+            'square_meters' => 'required|min:1|max:99999|numeric',
+            'users_id' => 'nullable|exists:users,id',
+        ];
+    }
+
+    protected function getValidationRulesEdit() {
+        // Creo le Validazioni per i campi nel form
+        return [
+            'title' => 'required|min:5|max:100',
+            'room_number' => 'required|min:1|max:999|numeric',
+            'bed_number' => 'required|min:1|max:999|numeric',
+            'bathroom' => 'required|min:1|max:999|numeric',
+            'address' => 'required|min:1|max:60000',
+            'price' => 'min:0|max:9999999|numeric|nullable',
+            'photo' => 'image|max: 1024|nullable',
             'description' => 'min:1|max:60000|nullable',
             'square_meters' => 'required|min:1|max:99999|numeric',
             'users_id' => 'nullable|exists:users,id',
