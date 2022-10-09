421a35a235e216dd34e6601f644d84e7ae315535
andrea-mangiapia <andrea.mangiapia@gmail.com>
2022-10-09 19:49:06 +0200

    add footer back-end

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,33 +23,33 @@
 </head>
 <body>
     <div id="app">
-  <header class="register_header">
-    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-        <div class="container">
-        
-            <figure class="logosize">
-                {{-- Testing --}}
-                <a href="http://localhost:8000/">
-                  <img src="{{ asset('storage/' . 'logo-boolbnb.png') }}" alt="BoolBnB">
-                </a>
-              </figure>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+      <header class="register_header">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+            
+                <figure class="logosize">
+                    {{-- Testing --}}
+                    <a href="http://localhost:8000/">
+                      <img src="{{ asset('storage/' . 'logo-boolbnb.png') }}" alt="BoolBnB">
+                    </a>
+                  </figure>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav mr-auto">
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
 
-                </ul>
-           </div>
-        </div>
-    </nav>
-  </header>
+                    </ul>
+              </div>
+            </div>
+        </nav>
+      </header>
 
-        <main class="py-4 artisanMain" >
-            @yield('content')
-        </main>
+      <main class="py-4 artisanMain" >
+          @yield('content')
+      </main>
     </div>
 </body>
 </html>
